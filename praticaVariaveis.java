290e1bb0bbd2343c92580b8ad4fe1aafe1b6b040
Raphael Martins Pimenta <pimentaraphael000@gmail.com>
2025-01-08 13:33:10 +0000

    primeiro commit, praticando enquanto trabalho na recepção.

---
+++ praticaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class praticaVariaveis {
+      public static void main(String[] args) throws Exception {
+
+        //Pratica usando int, string, double, etc.
+        
+       String nome = "Raphael";
+       String sobrenome = "Martins Pimenta"; 
+       int idade = 29;
+       double salario = 2500.5;
+       String java = "Java";
+
+      
+       System.out.println("Olá, me chamo " + nome + " " + sobrenome + ", tenho " + idade + " anos. Prazer te conhecer.");
+       System.out.println("Eu ganho um salário de " + salario + " sem os descostos.");
+       System.out.println("Estou aprendendo " + java + " a mais ou menos 1 mês.");
+        
+      }
+
+
+
+}
