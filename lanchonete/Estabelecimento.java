e84f2f996241ef7ae33fc8e4322aa1217583543b
Raphazelll <pimentaraphael000@gmail.com>
2025-01-09 17:47:50 -0300

    atualizando commit, estava duplicado.

--- lanchonete/Estabelecimento.java
+++ lanchonete/Estabelecimento.java
@@ -1,5 +1,30 @@
-package lanchonete;
+
+import lanchonete.area.cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.Almoxarife;
+import lanchonete.atendimento.cozinha.Cozinheiro;
 
 public class Estabelecimento {
+    public static void main(String[] args) {
+
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+        // AÇÕES QUE O ESTABELECIMENTO PRECISA TER CIÊNCIA.
+        cozinheiro.adicionarSucoNoBalcao();
+        cozinheiro.adicionarLancheBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Atendente atendente = new Atendente();
+
+        atendente.servindoMesa();
+        atendente.receberPagamento();
+
+        Cliente cliente = new Cliente();
+
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+    }
 
 }
