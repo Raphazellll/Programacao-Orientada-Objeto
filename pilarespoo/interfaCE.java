6b2b19a426d74fd5d88aebfdcb25594803717c05
Raphazelll <pimentaraphael000@gmail.com>
2025-01-11 20:37:43 -0300

    ultimo commit do dia

---
+++ pilarespoo/interfaCE.java
@@ -0,0 +1,37 @@
+package pilarespoo;
+
+public class interfaCE {
+
+    /*
+     * Interface
+     * 
+     * Como vimos anteriormente, Herança é um dos pilares de POO, mas uma
+     * curiosidade
+     * que se deve ser esclarecida na linguagem JAVA é que a mesma não permite o que
+     * conhecemos como Herança Múltipla.
+     * 
+     * Á medida que vão surgindo novas necessidades, novos equipamentos (objetos)
+     * nascem para atender
+     * as expectativas de oferecer ferramentas com finalidades bem específicas como
+     * por exemplo:
+     * Impressoras, Digitalizadoras, Copiadoras e etc.
+     * 
+     * Observem que não há uma especificação de amrca, modedlo e ou capacidades de
+     * execução das classes
+     * citadas acima, isto é o que consideramos o nível mais abstrato que
+     * denominamos como INTERFACES.
+     * 
+     * 
+     * Este é um dos maiores questionamentos dos desenvolvedores no que se refere a
+     * modelo de classe
+     * da aplicação
+     * 
+     * Como citada acima, JAVA não permite herança múltipla, logo, vamos imaginar
+     * que recebemos o desafio
+     * de projetar uma nova classe para criar objetos que representem as três
+     * características citadas
+     * acima e que iremos denominar de EquipamentoMultifuncional
+     * 
+     */
+
+}
