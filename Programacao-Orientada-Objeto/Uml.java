3f132b90557b35528a74cbacf8b0b25615da6d37
Raphazelll <pimentaraphael000@gmail.com>
2025-01-15 20:18:00 -0300

    atualizando aula uml

---
+++ Programacao-Orientada-Objeto/Uml.java
@@ -0,0 +1,84 @@
+public class Uml {
+
+    /*
+     * UML
+     * Linguagem de Modelagem Unificada ou UML, é uma notação que possibulia
+     * a representação gráfica do projeto.
+     * 
+     * Na UML, temos três conceitos necessários para compreendermos inicialmente:
+     * Diagramas, Elementos e Relacionamentos.
+     * 
+     * 
+     * Diagramas estruturais
+     * Diagrama de Classe: O Diagrama de Classes é utilizado para fazer a
+     * representação
+     * de estruturas de classes de negócio, interfaces e outros componentes do
+     * sistema.
+     * Por esta característica, este diagrama é considerado o mais importenta para
+     * UML,
+     * pois auxilia a maioria dos demais diagramas.
+     * 
+     * Diagrama de Objetos: Este diagrama representa os objetos existentes em um
+     * determinado
+     * instante ou fato na aplicação. Assim conseguimos ter uma perspectiva do
+     * estado de nossos
+     * objetos mediante a interação dos usuários no sistema.
+     * 
+     * Diagrama de Classe
+     * O diagrama de classes ilustra graficamente como classes serão estruturadas e
+     * interligadas
+     * entre si diante da proposta do nosso software.
+     * Em diagrama e estrutura das classes é constituída por:
+     * 
+     * Identificação: Nome e/ou finalidaded da classe
+     * 
+     * Atributos: Propriedadeds e/ou características
+     * 
+     * Operações: Ações e/ou métodos
+     * 
+     * Relacionamentos:
+     * Em um diagrama as classes podem existir de forma independente, mas obviamente
+     * haverá em
+     * alguma etapa da aplicação e necessidade ded algumas se relacionarem, o que
+     * devemos
+     * compreender é o nível de dependência entre elas:
+     * 
+     * Associação: Uma associação define um relacionamento entre duas classes,
+     * permitindo que
+     * um objeto tenha acesso a estrutura de um outro objeto.
+     * 
+     * Agregação: Em uma agragação a classe principal contém uma relação com outra
+     * classe mas
+     * ela pode existir sem a classe agregadora. Imagima em um cadastro de
+     * Candidatos, podemos
+     * encontrar candidatos que ainda não possuam uma Profissão.
+     * 
+     * Composição: A composição já caracterizaz uma dependência existencial entre a
+     * classe principal
+     * e a classe associada. Imaginamos que uma Admissão só poderá existir contendo
+     * suas informações
+     * básicas e a composição do Candidato selecionado.
+     * 
+     * Multiplicidade: Nem sempre o relacionamento entre as classes será de UM PARA
+     * UM, um determinado
+     * cenário poderá exigir multiplicidades específicas conforme opções apaixo:
+     * 
+     * 1. Representa uma associação contendo um elemento.
+     * *. Representa uma associação contendo uma lista de elementos.
+     * 0..1 Representa uma associação contendo zero ou um elemento.
+     * 0..* Representa uma associação contendo zero ou uma lista de elementos.
+     * 1..* Representa uma associação contendo um ou uma lista de elementos.
+     * 
+     * Visibilidade: Os atributos e métodos de uma classe podem receber níveis de
+     * visibilidade
+     * na UML existem símbolos que representam cada um deles.
+     * 
+     * (+) Visibilidade pública
+     * (#) Visibilidade protegiga (muita associada com herança)
+     * (-) Visibilidade privada
+     * 
+     * 
+     * 
+     * 
+     */
+}
